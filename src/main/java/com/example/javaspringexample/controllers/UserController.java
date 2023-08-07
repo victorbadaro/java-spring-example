49ad0658b3af593b6bc8c6c861ff68b2cadaf852
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-08-07 10:23:58 -0300

    feat: add method to delete a specific user

--- src/main/java/com/example/javaspringexample/controllers/UserController.java
+++ src/main/java/com/example/javaspringexample/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,19 @@ public class UserController {
       User updatedUser = userRepository.save(foundUser.get());
 
       return ResponseEntity.ok().body(updatedUser);
+    }
+
+    return ResponseEntity.notFound().build();
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> delete(@PathVariable Long id) {
+    Optional<User> foundUser = userRepository.findById(id);
+
+    if (foundUser.isPresent()) {
+      userRepository.deleteById(id);
+
+      return ResponseEntity.noContent().build();
     }
 
     return ResponseEntity.notFound().build();
