bd7e6eb67289dce132a542004b303795c7f6e75e
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-08-07 09:30:19 -0300

    feat: add user model

---
+++ src/main/java/com/example/javaspringexample/models/User.java
@@ -0,0 +1,23 @@
+package com.example.javaspringexample.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @NotNull
+  @Column(nullable = false)
+  private String name;
+}
