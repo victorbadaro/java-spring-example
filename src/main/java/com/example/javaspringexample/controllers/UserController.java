2c45d4ef4c93a84d918927634d869ef6f804dfc3
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-08-07 09:44:30 -0300

    feat: add method to create users

--- src/main/java/com/example/javaspringexample/controllers/UserController.java
+++ src/main/java/com/example/javaspringexample/controllers/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +24,11 @@ public class UserController {
   @ResponseStatus(HttpStatus.OK)
   public List<User> find() {
     return userRepository.findAll();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public User create(@RequestBody User user) {
+    return userRepository.save(user);
   }
 }
