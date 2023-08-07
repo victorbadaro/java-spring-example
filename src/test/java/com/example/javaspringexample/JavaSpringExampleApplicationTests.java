51444326edcda80b1b991232f21adaf47f639809
Victor Badaró <victor.badaro@sensedia.com>
2023-08-06 21:37:46 -0300

    first commit

---
+++ src/test/java/com/example/javaspringexample/JavaSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
